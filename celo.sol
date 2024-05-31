5548905322c3768b672773f3ac6cf79d62026568
candra.eth <160713703+candraETH@users.noreply.github.com>
2024-05-31 11:03:48 +0700

    Add files via upload

---
+++ celo.sol
@@ -0,0 +1,18 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract HelloWorld {
+    string public message;
+
+    constructor() {
+        message = "Hello, World!";
+    }
+
+    function setMessage(string memory newMessage) public {
+        message = newMessage;
+    }
+
+    function getMessage() public view returns (string memory) {
+        return message;
+    }
+}
